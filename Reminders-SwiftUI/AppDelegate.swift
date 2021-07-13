178a7b0f92d4ac2389a7d7a54cf9adad3bac0791
Mariana <mariana@fastmail.net>
2021-07-13 09:01:26 +0200

    Set up the core data stack

--- Reminders-SwiftUI/AppDelegate.swift
+++ Reminders-SwiftUI/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,4 +9,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Use this method to select a configuration to create the new scene with.
     return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
+  
+  
+  // MARK: - Core Data Stack -
+  lazy var persistentContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: "Reminders")
+    // loadPersistentStores takes a completion block with two arguments:
+    // storeDescription - an instance of NS persistent store description - it is an object that allows you to customize, create and load the underlying store
+    // error - value if the creation of this stack failed
+    // we won't be doing any customizing, so we can ignore the first argument
+    container.loadPersistentStores { (storeDescription, error ) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    return container
+  }()
 }
